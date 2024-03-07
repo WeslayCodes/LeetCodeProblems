5b3053c0840a07f74d49fe9c1487776f101fed81
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2024-03-07 08:04:04 -0600

    Added comments to UniquePaths.java

--- UniquePaths/src/UniquePaths.java
+++ UniquePaths/src/UniquePaths.java
@@ -14,6 +14,7 @@ class UniquePaths {
         m--;
         n--;
 
+        // Swap if n is greater to ensure combination properly calculated
         if (m < n) {
             m = m + n;
             n = m - n;
@@ -22,6 +23,7 @@ class UniquePaths {
 
         long result = 1;
 
+        // Combinational logic
         for (int i=m+1, j=1; i<=m+n; i++, j++) {
             result *= i;
             result /= j;
@@ -30,3 +32,34 @@ class UniquePaths {
         return (int) result;
     }
 }
+
+/**
+ * UniquePaths.java
+ *
+ * Calculates the number of unique paths for an empty grid.
+ * Start at top left, end at bottom right.
+ * Solved using Dynamic Programming.
+ */
+//class UniquePaths {
+//    public int uniquePaths(int m, int n) {
+//        int[][] grid = new int[m][n];
+//
+//        for (int i=0; i<m; i++) {
+//            for (int j=0; j<n; j++) {
+//                if (i == 0) {
+//                    grid[0][j] = 1;
+//                    continue;
+//                }
+//
+//                if (j == 0) {
+//                    grid[i][0] = 1;
+//                    continue;
+//                }
+//
+//                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+//            }
+//        }
+//
+//        return grid[m-1][n-1];
+//    }
+//}
