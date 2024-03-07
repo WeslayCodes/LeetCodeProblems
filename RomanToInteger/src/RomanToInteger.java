368c44f782ddc8ac3c351a974c1b18d04b6a1c82
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2024-03-06 18:15:15 -0600

    Commented RomanToInteger.java

--- RomanToInteger/src/RomanToInteger.java
+++ RomanToInteger/src/RomanToInteger.java
@@ -1,12 +1,18 @@
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * RomanToInteger.java
+ *
+ * Converts a roman numeral string into an integer.
+ */
 class RomanToInteger {
     public int romanToInt(String s) {
         int lastCharPos = s.length() - 1;
         int sum = 0;
-        Map<Character, Integer> nums = new HashMap<>();
 
+        // Map of roman numerals with associated integer value
+        Map<Character, Integer> nums = new HashMap<>();
         nums.put('I', 1);
         nums.put('V', 5);
         nums.put('X', 10);
@@ -17,6 +23,7 @@ class RomanToInteger {
 
         int lastVal = nums.get(s.charAt(lastCharPos));
 
+        // Loop through each character and add/subtract value character is associated with
         for (int i=0; i<lastCharPos; i++) {
             int nextVal = nums.get(s.charAt(i+1));
             int curVal = nums.get(s.charAt(i));
